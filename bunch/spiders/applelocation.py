88208a13d6ba62ecf5c41fe1a965976689ecb33c
Alexandr Nesterenko <alexandr.nesterenko@softswiss.com>
2014-12-22 22:39:52 +0300

    apple spider is tested

--- bunch/spiders/applelocation.py
+++ bunch/spiders/applelocation.py
@@ -15,7 +15,6 @@ class AppleLocationSpider(Spider):
     
     meta_first = 'apple_location_spider_first'
     meta_country = 'apple_location_spider_country'
-    meta_url = 'apple_location_spider_url'
     
     hours_countries = ['United States']
     hours_full_year = '24/7, 365 days a year'  
@@ -29,8 +28,8 @@ class AppleLocationSpider(Spider):
             for menuitem in response.xpath('//section[@id="country_switcher"]/div/ul/li'):
                 code = menuitem.xpath('@data-tag')[0].extract()
                 name = menuitem.xpath('text()')[0].extract()
-                for link in [l for l in LinkExtractor(restrict_xpaths='//div[@id="%s"]/div/ul/li/a' % (code+'stores')).extract_links(response)][:1]:
-                    yield Request(link.url, callback=self.parse, meta={self.meta_country: name, self.meta_url: link.url})
+                for link in [l for l in LinkExtractor(restrict_xpaths='//div[@id="%s"]/div/ul/li/a' % (code+'stores')).extract_links(response)]:
+                    yield Request(link.url, callback=self.parse, meta={self.meta_country: name})
                     
         else: #scraping of items
             item = LocationItem()
@@ -46,36 +45,44 @@ class AppleLocationSpider(Spider):
             item['services'] = response.xpath('//nav[@class="nav hero-nav selfclear"]//img/@alt').extract()
             state = address.xpath('.//span[@class="region"]/text()').extract()
             if len(state): item['state'] = state[0]
-            #todo: find store_email if it exists
-            #todo: find store_floor_plan_url if it exists
+            #store_email: not found
+            #store_floor_plan_url: not found
             item['store_image_url'] = address.xpath('../../div[@class="column last"]/img/@src')[0].extract()
             item['store_name'] = address.xpath('div[@class="store-name"]/text()')[0].extract().strip()
-            #todo: find store_id if it exists
-            #todo: find weekly_ad_url if it exists
-            item['store_id'] = item['weekly_ad_url'] = item['store_url'] = response.meta[self.meta_url]
+            item['store_id'] = response.xpath('/html/head/meta[@name="omni_page"]/@content').re('(R\d+)$')[0]
+            #find weekly_ad_url: on the same page
+            item['weekly_ad_url'] = item['store_url'] = response.url
             zipcode = address.xpath('.//span[@class="postal-code"]/text()').extract()
             if len(zipcode): item['zipcode'] = zipcode[0]
             yield item
             
     def parse_hours(self, trs):
         trs = trs[1:]
-        trs = [(tr.xpath('td/text()')[0].extract(), tr.xpath('td/text()')[1].extract()) for tr in trs]
-        
-        if trs[0][0] == self.hours_full_year:
-            return days_dict([('12:00 a.m','12:00 a.m') for x in range(7)])
+        rows = [tr.xpath('td/text()') for tr in trs]
+
+        if rows[0][0].extract() == self.hours_full_year:
+            return get_hours_item_value([('12:00 a.m','12:00 a.m') for x in range(7)])
         else:
             sitedays = ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            
             days = {}
-            for tr in trs:
-                idxes = None
-                if ' - ' in tr[0]: #interval of days
-                    words = re.split('\W+', tr[0])
-                    start = words[0]
-                    end = words[1]
-                    idxes = range(sitedays.index(start), sitedays.index(end)+1)
+            for row in rows:
+                day_interval = row[0]
+                time_interval = row[1].extract()
+                
+                day_idxes = None
+                pieces = day_interval.re('(\w+) - (\w+)')
+                if pieces: #interval of days
+                    day_idxes = range(sitedays.index(pieces[0]), sitedays.index(pieces[1])+1)
                 else: #single day
-                    words = re.split('\W+', tr[0])
-                    idxes = [sitedays.index(words[0])]
-                for i in idxes:
-                    days[i] = tr[1].split(' - ')
+                    d = day_interval.re('\w+')[0]
+                    day_idxes = [sitedays.index(d)]
+                
+                if ' - ' in time_interval:
+                    time_interval = time_interval.split(' - ')
+                elif time_interval == 'Closed':
+                    continue
+                     
+                for i in day_idxes:
+                    days[i] = time_interval
             return get_hours_item_value(days)
